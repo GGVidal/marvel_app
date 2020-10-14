a692fdd554ac914ec70fdcc88c07e6902bef432c
GabrielGondim98 <gabrielvidalgondim@gmail.com>
2020-10-14 18:55:16 -0300

    CHG: Editing style

--- src/pages/SearchHero.js
+++ src/pages/SearchHero.js
@@ -25,32 +25,33 @@ const SearchHero = () => {
     setChar(charObj);
   };
   return (
-    <View style={styles.container}>
-      <View style={{flexDirection: 'column', width: 350}}>
+    <>
+      <View style={styles.container}>
         <Input
-          onEndEditing={(e) => {
+          onSubmitEditing={(e) => {
             fetchChar(e.nativeEvent.text);
           }}
-          inputStyle={{color: '#fdfcfa'}}
+          containerStyle={{width: 330}}
+          inputStyle={{color: '#4d4b50'}}
           placeholder="Type your hero's name"
           placeholderTextColor="#abafae"
           rightIcon={
             <Button
               type="clear"
-              icon={<Icon name="search" size={24} color="#fdfcfa" />}
+              icon={<Icon name="search" size={24} color="#100c08" />}
             />
           }
         />
       </View>
       {Object.keys(char).length ? (
-        <View>
-          <Card containerStyle={{backgroundColor: '#fdfcfa', borderRadius: 10}}>
+        <View style={styles.infoContainer}>
+          <Card containerStyle={{backgroundColor: '#F8F8FF', borderRadius: 10}}>
             <Card.Title>
               <Text h4>{char.name}</Text>
             </Card.Title>
             <Card.Divider />
             <Card.Image
-              style={{borderRadius: 10}}
+              style={{borderRadius: 10, marginBottom: 10}}
               resizeMode="contain"
               source={{uri: char.charImage}}
               PlaceholderContent={<ActivityIndicator />}
@@ -58,18 +59,21 @@ const SearchHero = () => {
             {/* test in 2G */}
             <Text style={{marginBottom: 10}}>{char.description}</Text>
             <Button
+              type="outline"
               buttonStyle={{
                 borderRadius: 0,
                 marginLeft: 0,
                 marginRight: 0,
                 marginBottom: 0,
+                borderColor: '#F8F8FF',
               }}
-              title="VIEW NOW"
+              title="CHECK THE COMICS"
+              titleStyle={{color: '#cf352e'}}
             />
           </Card>
         </View>
       ) : null}
-    </View>
+    </>
   );
 };
 
@@ -77,7 +81,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    backgroundColor: '#F8F8FF',
+    borderRadius: 10,
+    maxHeight: 80,
   },
+  infoContainer: {marginBottom: 80}
 });
 
 export default {backgroundColor: '#a61202', title: <SearchHero />};
