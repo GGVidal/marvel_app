970e418df9b51d6bc89c7769702e6e27a7617b20
GabrielGondim98 <gabrielvidalgondim@gmail.com>
2020-10-16 15:33:24 -0300

    NEW: Testing mapview in the page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,34 @@ import React from 'react';
 import Intro from './Intro';
 import SearchHero from './SearchHero';
 import Onboarding from 'react-native-onboarding-swiper';
+import {View, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const Main = ({navigation}) => {
+  const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+  });
+  const region = {
+    latitude: -3.743616,
+    longitude: -38.535392,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+  };
+
+  const latLong = {
+    latitude: -3.743616,
+    longitude: -38.535392,
+  };
+
   return (
     <Onboarding
       showSkip={false}
@@ -16,7 +42,24 @@ const Main = ({navigation}) => {
         },
         {
           backgroundColor: '#a61202',
-          title: <SearchHero navigation={navigation}/>,
+          title: <SearchHero navigation={navigation} />,
+        },
+        {
+          backgroundColor: '#a61202',
+          title: (
+            <View style={styles.container}>
+              <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={region}>
+                <Marker
+                  coordinate={latLong}
+                  title="Title"
+                  description="description"
+                />
+              </MapView>
+            </View>
+          ),
         },
       ]}
     />
